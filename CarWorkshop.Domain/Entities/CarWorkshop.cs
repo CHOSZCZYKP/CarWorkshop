7907d633dce5c8b67997eef3b435d300d9b4fada
Pawel <Pawel@DESKTOP-DTPH9NG>
2025-03-23 13:39:29 +0100

    Przygotowałem infrastrukturę (konfiguracja z bazą danych, seedowanie danych)

--- CarWorkshop.Domain/Entities/CarWorkshop.cs
+++ CarWorkshop.Domain/Entities/CarWorkshop.cs
@@ -8,11 +8,13 @@ namespace CarWorkshop.Domain.Entities
 {
     public class CarWorkshop
     {
-        public required int Id { get; set; }
+        public int Id { get; set; }
         public string Name { get; set; } = default!;
         public string? Description { get; set; }
         public DateTime CreateAt { get; set; } = DateTime.UtcNow;
         public CarWorkshopContactDetails ContactDetails { get; set; } = default!;
+
+        public string? About { get; set; }
 
         public string EncodedName { get; private set; } = default!;
 
--- CarWorkshop/Program.cs
+++ CarWorkshop/Program.cs
@@ -1,10 +1,22 @@
+using CarWorkshop.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using CarWorkshop.Infrastructure.Extensions;
+using CarWorkshop.Infrastructure.Seeders;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+
+builder.Services.AddInfrastructure(builder.Configuration);
+
 var app = builder.Build();
 
+var scope = app.Services.CreateScope();
+
+var seeder = scope.ServiceProvider.GetRequiredService<CarWorkshopSeeder>();
+await seeder.Seed();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
